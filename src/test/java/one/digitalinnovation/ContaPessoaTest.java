a7bf50aa49b7915b5529fc55ea5c95840b81485c
”murilovalentim” <murilovalentim01@gmail.com>
2024-07-15 19:59:34 -0300

    Adicionando comentario para um melhor entendimento dos testes

--- src/test/java/one/digitalinnovation/ContaPessoaTest.java
+++ src/test/java/one/digitalinnovation/ContaPessoaTest.java
@@ -11,22 +11,22 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 
 public class ContaPessoaTest {
-    @Test
+    @Test // -> Anotação primordial para realizar um teste
     void calcularSaldo() {
         ContaPessoa contaPessoa = new ContaPessoa("Murilo", 18, 200);
         contaPessoa.depositar(100);
         Assertions.assertEquals(300, contaPessoa.getSaldo());
     }
-    @Test
+    @Test // -> Anotação primordial para realizar um teste
     void verificarAnoDoVeiculo(){
         Veiculo Golzinho = new Veiculo("Gol", LocalDate.of(2000, 1, 1));
         Assertions.assertEquals(24, Golzinho.getAno());
     }
-    @Test
+    @Test // -> Anotação primordial para realizar um teste
     void verificarOverificador(){
-        Veiculo Golzinho = new Veiculo("Gol", LocalDate.of(2000, 1, 1));
-        Assertions.assertFalse(Golzinho.menorDeVinteAnos());
+        Veiculo Golzinho = new Veiculo("Gol", LocalDate.of(2000, 1, 1)); //Cria um cenario
+        Assertions.assertFalse(Golzinho.menorDeVinteAnos()); // Executa as validacoes
 
-        Assertions.assertTrue(Golzinho.passouDeVinteAnos());
+        Assertions.assertTrue(Golzinho.passouDeVinteAnos()); // Executa as validacoes
     }
 }
